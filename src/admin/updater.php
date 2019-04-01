982370729c1178e0c9c8cf1aa6d139294d95c504
seothemes <seothemeswp@gmail.com>
2019-04-01 20:12:12 +0800

    Update plugin updater URL

--- src/admin/updater.php
+++ src/admin/updater.php
@@ -3,7 +3,7 @@
 namespace GenesisCustomizer;
 
 $genesis_customizer_updater = \Puc_v4_Factory::buildUpdateChecker(
-	'https://github.com/genesiscustomizer/plugin',
+	'https://github.com/seothemes/genesis-customizer-plugin',
 	_get_path() . 'genesis-customizer.php',
 	_get_handle()
 );
