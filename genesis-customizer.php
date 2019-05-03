e29e70ea613ac9897c2d2d0b2405cfae30667558
seothemes <seothemeswp@gmail.com>
2019-05-03 09:43:37 +0800

    Bump version

--- genesis-customizer.php
+++ genesis-customizer.php
@@ -3,7 +3,7 @@
  * Genesis Customizer
  *
  * Plugin Name: Genesis Customizer
- * Version:     0.3.0
+ * Version:     0.3.1
  * Text Domain: genesis-customizer
  * Plugin URI:  https://genesiscustomizer.com/
  * Description: Core functionality plugin for the Genesis Customizer theme.
